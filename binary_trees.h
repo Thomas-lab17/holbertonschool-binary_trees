c281a5e68da99ae2e8c7102f09ca5d9c635f4960
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2026-04-09 15:13:18 +0200

    feat: add binary_tree_preorder function for pre-order traversal

--- binary_trees.h
+++ binary_trees.h
@@ -23,5 +23,6 @@ typedef struct binary_tree_s
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 
 #endif /* _BINARY_TREES_H_ */
