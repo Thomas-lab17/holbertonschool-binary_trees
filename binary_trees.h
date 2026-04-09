c1740755d70c2001a8b0795f8e33c7c4ee66cb08
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2026-04-09 15:15:12 +0200

    feat: add binary_tree_height function to measure the height of a binary tree

--- binary_trees.h
+++ binary_trees.h
@@ -24,5 +24,6 @@ void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+size_t binary_tree_height(const binary_tree_t *tree);
 
 #endif /* _BINARY_TREES_H_ */
