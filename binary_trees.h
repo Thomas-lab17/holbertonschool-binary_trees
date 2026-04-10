808db3350c4978d688d0813ceb008f306c85a6ec
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2026-04-10 11:10:28 +0200

    feat: add binary_tree_uncle function to find the uncle of a node

--- binary_trees.h
+++ binary_trees.h
@@ -27,5 +27,6 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
 #endif /* _BINARY_TREES_H_ */
